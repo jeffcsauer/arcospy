b03503c8179ed7976ea26b9088e2d00f0ceddcaa
jeffcsauer <jeffery.sauer@mail.mcgill.ca>
2019-10-25 22:00:20 -0400

    figuring out file paths within travis...

--- test_majority.py
+++ test_majority.py
@@ -9,49 +9,49 @@ input_key = 'WaPo'
 
 # Test function 1: pharm_latlon
 func1_output = pharm_latlon(input_county, input_state, input_key)
-func1_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func1_output.csv') 
+func1_output_R = pd.read_csv('/arcospy/tests_datasets/func1_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func1_output_R = func1_output_R.drop(func1_output_R.columns[0], axis=1)
 
 # Test function 3: pharm_tracts
 func3_output = pharm_tracts(input_county, input_state, input_key)
-func3_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func3_output.csv') 
+func3_output_R = pd.read_csv('/arcospy/tests_datasets/func3_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func3_output_R = func3_output_R.drop(func3_output_R.columns[0], axis=1)
 
 # Test function 5: buyer_addresses
 func5_output = buyer_addresses(input_county, input_state, input_key)
-func5_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func5_output.csv') 
+func5_output_R = pd.read_csv('/arcospy/tests_datasets/func5_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func5_output_R = func5_output_R.drop(func5_output_R.columns[0], axis=1)
 
 # Test function 7: county_population
 func7_output = county_population(input_county, input_state, input_key)
-func7_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func7_output.csv') 
+func7_output_R = pd.read_csv('/arcospy/tests_datasets/func7_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func7_output_R = func7_output_R.drop(func7_output_R.columns[0], axis=1)
 
 # Test function 9: not_pharmacies
 func9_output = not_pharmacies(input_key)
-func9_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func9_output.csv') 
+func9_output_R = pd.read_csv('/arcospy/tests_datasets/func9_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func9_output_R = func9_output_R.drop(func9_output_R.columns[0], axis=1)
 
 # Test function 11: buyer_details
 func11_output = buyer_details(input_county, input_state, input_key)
-func11_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func11_output.csv') 
+func11_output_R = pd.read_csv('/arcospy/tests_datasets/func11_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func11_output_R = func11_output_R.drop(func11_output_R.columns[0], axis=1)
 
 # Test function 13: total_manufacturers_county
 func13_output = total_manufacturers_county(input_county, input_state, input_key)
-func13_output_R = pd.read_csv('/Users/jefferysauer/Dropbox/Maryland/PhD_Courses/GEOG788P/jeffsauer_MnM4SDS_project/arcospy/arcospy/tests_datasets/func13_output.csv') 
+func13_output_R = pd.read_csv('/arcospy/tests_datasets/func13_output.csv') 
 
 # Drop first index column as artifact of importing csv
 func13_output_R = func13_output_R.drop(func13_output_R.columns[0], axis=1)
